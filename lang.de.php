0df1073a8a78362ab8578b2144077984ce49f8ac
Nils Hörrmann <post@nilshoerrmann.de>
2009-02-16 17:30:22 +0100

    new files for search and translations

---
+++ lang.de.php
@@ -0,0 +1,71 @@
+<?php
+
+$about = array (
+  'name' => 'Deutsch (formell)',
+);
+
+$dictionary = array (
+
+  'Mediathek' =>
+  'Mediathek',
+
+  'Connected Upload Field' =>
+  'Verknüpftes Uploadfeld',
+
+  'None Selected' =>
+  '',
+
+  'Filter by Tags or Categories' =>
+  'Nach Tags oder Kategorien filtern',
+
+  'Comma Separated List' =>
+  'kommagetrennte Liste',
+
+  'A file upload field must be selected.' =>
+  'Es muss ein Dateiuploadfeld ausgewählt sein.',
+
+  'Create new entry in related section' =>
+  'Neuen Eintrag im verknüpften Bereich erstellen',
+
+  'Use comma separated entry ids for filtering.' =>
+  'Filtern Sie das Ergebnis mittels kommagetrennter IDs',
+  
+  'None Selected' =>
+  'Keine Auswahl',
+
+  'Show Selected' =>
+  'Auswahl einblenden',
+
+  'Show All' =>
+  'Alle einblenden',
+
+  'Search' =>
+  'Suche',
+
+  'Preview' =>
+  'Vorschau',
+
+  'Drop Item' =>
+  'Einfügen',
+
+  'Create New' =>
+  'Neu erstellen', 
+
+  'Multiple Choise' =>
+  'Mehrfachauswahl',
+
+  'Single Choise' =>
+  'Einfachauswahl',
+  
+  '%s Show file count instead of file names in entry overview' =>
+  '%s Zeige die Anzahl anstelle der Dateinamen in der Übersicht der Einträge.',
+  
+  'file' =>
+  'Datei',
+  
+  'files' =>
+  'Dateien',
+
+);
+
+?>
